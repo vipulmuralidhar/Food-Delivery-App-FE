f441631f02d88fb6b03b2dc217b2171b82803352
vipulm <vipulmuralidhar@gmail.com>
2024-11-07 01:52:20 -0600

    Added new jenkins file and pom updates with plugins

--- src/app/constants/url.ts
+++ src/app/constants/url.ts
@@ -3,4 +3,4 @@
 // export const API_URL_FC = 'http://localhost:9092';
 // export const API_URL_UD = 'http://localhost:9093';
 
-export const k8ExternalIp ='http://k8s-default-awsingre-9aa4c55379-1173899711.us-east-1.elb.amazonaws.com'; 
+export const k8ExternalIp ='http://k8s-default-awsingre-9aa4c55379-788229990.us-east-1.elb.amazonaws.com'; 
