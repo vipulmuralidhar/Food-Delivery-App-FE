fa44a8547babc3dc4e962db181de9ea8c6eb9256
vipulmuralidhar <vipulmuralidhar@gmail.com>
2024-11-07 19:59:49 -0600

    Update url.ts
    updating correct url for load balancer

--- src/app/constants/url.ts
+++ src/app/constants/url.ts
@@ -3,5 +3,5 @@
 // export const API_URL_FC = 'http://localhost:9092';
 // export const API_URL_UD = 'http://localhost:9093';
 
-export const k8ExternalIp ='k8s-default-eurekaaw-a1844d02e1-601441574.us-east-1.elb.amazonaws.com'; 
-//http://k8s-default-awsingre-9aa4c55379-788229990.us-east-1.elb.amazonaws.com
+export const k8ExternalIp ='http://k8s-default-awsingre-9aa4c55379-788229990.us-east-1.elb.amazonaws.com'; 
+
